23143bb1e2464b89340aa290104ef5d7785468e4
DinmukhamedAppaz <152635151+dimash012@users.noreply.github.com>
2026-07-08 09:45:46 +0500

    First commit

---
+++ src/test/java/com/configurationpc/PCConfigurator/PcConfiguratorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.configurationpc.PCConfigurator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PcConfiguratorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
